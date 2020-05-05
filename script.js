f52e0dd34b8ca9f57d3069e06e61fe081fc87b68
Dmitrii Kuzmin <63294836+ApplY3D@users.noreply.github.com>
2020-05-05 12:23:47 +0200

    added support for change active tab class

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
-function tabs(wrapNavigationSelector,tabsBtnSelector,tabsContentSelector, showClassName, hideClassName){
+function tabs(wrapNavigationSelector,tabsBtnSelector,tabsContentSelector, showClassName, hideClassName, activeClassName){
     let tabWrap = document.querySelector(wrapNavigationSelector),
         tab = document.querySelectorAll(tabsBtnSelector),
         tabContent = document.querySelectorAll(tabsContentSelector),
         show = showClassName || 'show',
-        hide = hideClassName || 'hide';
+        hide = hideClassName || 'hide',
+        active = activeClassName || 'active';
     
+        hideTabContent(1);
+        activeTab(0);
+
     function hideTabContent(a){
         for(let i=a; i<tabContent.length;i++){
             tabContent[i].classList.remove(show);
             tabContent[i].classList.add(hide);
         }
     }
-    hideTabContent(1);
 
     function showTabContent(b){
         if (tabContent[b].classList.contains(hide)){
             tabContent[b].classList.remove(hide);
             tabContent[b].classList.add(show);
         }
+    }
+
+    function inactiveTabs(a){
+        for(let i=a; i<tabContent.length;i++){
+            tab[i].classList.remove(active);
+        }
+    }
+
+    function activeTab(b){
+        tab[b].classList.add(active);
     }
 
     tabWrap.addEventListener('click', function(event){
@@ -27,6 +40,8 @@ function tabs(wrapNavigationSelector,tabsBtnSelector,tabsContentSelector, showCl
                 if (target==tab[i]){
                     hideTabContent(0);
                     showTabContent(i);
+                    inactiveTabs(0);
+                    activeTab(i);
                     break;
                 }
             }
